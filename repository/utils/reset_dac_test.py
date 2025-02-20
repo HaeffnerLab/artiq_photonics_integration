fec2838f92c9172b07c6a4c5e42ba887cecd5d39
Bingran You <bingran.you@berkeley.edu>
2025-02-20 13:53:43 -0800

    set voltage test reset

--- repository/utils/reset_dac_test.py
+++ repository/utils/reset_dac_test.py
@@ -11,8 +11,9 @@ class ResetDACTest(EnvExperiment):
         self.setattr_device("core")
         self.setattr_device("zotino0")
 
+        # 32 channels, 0.1 V for first channel, 0.2 V for second channel, ..., 0.32 V for 32nd channel
         self.channels = [i for i in range(32)]
-        self.voltages = [0.1] * 32
+        self.voltages = [0.1 + 0.1 * i for i in range(32)]
 
 
     @kernel
